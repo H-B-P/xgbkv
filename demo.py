884a14ec1212bd82a8f9f81056d50cb5510bb673
no no <no@no.no>
2018-08-19 23:04:05 +0100

    add classifier

--- demo.py
+++ demo.py
@@ -5,15 +5,29 @@ import xgbkv
 
 fullDF = pd.read_csv("iris.csv")
 
-varsDF = fullDF[["sepal.length", "sepal.width", "petal.length"]]
-respDF = fullDF[["petal.width"]]
+#REG
+
+#varsDF = fullDF[["sepal.length", "sepal.width", "petal.length"]]
+#respDF = fullDF[["petal.width"]]
+#
+#DM = xgb.DMatrix(varsDF, label=respDF)
+#parameters={"max_depth":1, "learning_rate":0.1}
+#
+#a = xgbkv.XGBKVRegressor(parameters,  DM,  100, metrics=["min", "max", "rmse",  "mae"])
+#
+#a.predict(DM)
+#
+#b = xgbkv.XGBKVRegressor(parameters,  DM,  100, 3,   metrics=["quintiles",  "mae"])
+
+#CLASS
+
+varsDF = fullDF[["sepal.length", "sepal.width", "petal.length", "petal.width"]]
+respDF = fullDF[["variety"]]
+
+respDF["variety"] = respDF["variety"].map({"Setosa":0,"Versicolor":1, "Virginica":2})
 
 DM = xgb.DMatrix(varsDF, label=respDF)
-parameters={"max_depth":1, "learning_rate":0.1}
 
-a = xgbkv.XGBKVRegressor(parameters,  DM,  100, metrics=["min", "max", "rmse",  "mae"])
+parameters={"max_depth":2, "learning_rate":0.1,  "objective":"multi:softprob",  "num_class":3}
 
-a.predict(DM)
-
-b = xgbkv.XGBKVRegressor(parameters,  DM,  100, 3,   metrics=["quintiles",  "mae"])
-
+c=xgbkv.XGBKVClassifier(parameters,  DM,  30)
--- xgbkv.py
+++ xgbkv.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
+#~~~REGRESSOR~~~
+
 def provide_average(pred,  act):
     return(np.mean(pred))
 
@@ -18,42 +20,37 @@ def provide_max(pred, act):
 def provide_rmse(pred, act):
     return(np.sqrt(sum((pred-act)*(pred-act))/len(pred)))
 
-def provide_Xile_pred_provider(n, N):
+def provide_Xile_pred_provider(n, X):
     def provide_Xile_pred(pred, act):
         inds=pred.argsort()
         sortedPred=pred[inds]
-        quintileSubset=sortedPred[len(pred)*(n-1)/N:len(pred)*n/N]
-        return sum(quintileSubset)/len(quintileSubset)
+        XileSubset=sortedPred[len(pred)*(n-1)/X:len(pred)*n/X]
+        return sum(XileSubset)/len(XileSubset)
     return provide_Xile_pred
 
-def provide_Xile_act_provider(n, N):
+def provide_Xile_act_provider(n, X):
     def provide_Xile_act(pred, act):
         inds=pred.argsort()
         sortedAct=act[inds]
-        quintileSubset=sortedAct[len(pred)*(n-1)/N:len(pred)*n/N]
-        return sum(quintileSubset)/len(quintileSubset)
+        XileSubset=sortedAct[len(pred)*(n-1)/X:len(pred)*n/X]
+        return sum(XileSubset)/len(XileSubset)
     return provide_Xile_act
 
-metrics1DLookup={
-"quintiles":["qu1 pred", "qu2 pred", "qu3 pred", "qu4 pred",  "qu5 pred",  "qu1 act",  "qu2 act",  "qu3 act",  "qu4 act",  "qu5 act"]}
+regMetrics1DLookup={
+"quintiles":["qui1 pred", "qui2 pred", "qui3 pred", "qui4 pred",  "qui5 pred",  "qui1 act",  "qui2 act",  "qui3 act",  "qui4 act",  "qui5 act"]
+}
 
-evalLookup={
+regEvalLookup={
 "average":provide_average, 
 "mae":provide_mae, 
 "min":provide_min, 
 "max":provide_max, 
-"rmse":provide_rmse, 
-"qu1 pred":provide_Xile_pred_provider(1, 5), 
-"qu2 pred":provide_Xile_pred_provider(2, 5), 
-"qu3 pred":provide_Xile_pred_provider(3, 5), 
-"qu4 pred":provide_Xile_pred_provider(4, 5),  
-"qu5 pred":provide_Xile_pred_provider(5, 5), 
-"qu1 act":provide_Xile_act_provider(1, 5), 
-"qu2 act":provide_Xile_act_provider(2, 5), 
-"qu3 act":provide_Xile_act_provider(3, 5), 
-"qu4 act":provide_Xile_act_provider(4, 5), 
-"qu5 act":provide_Xile_act_provider(5, 5), 
+"rmse":provide_rmse,
 }
+
+for i in range(1, 5+1):
+    regEvalLookup["qui"+str(i)+" pred"] = provide_Xile_pred_provider(i, 5)
+    regEvalLookup["qui"+str(i)+" act"] = provide_Xile_act_provider(i, 5)
 
 def XGBKVRegressor(params,  trainData,  nRounds, evalRounds=10,  metrics=["average", "mae", "min", "max",  "rmse"],  testData=None, outputs=["terminal","csv",  "graphs"], csvName="output.csv",  graphPrefix="output", ideals=True):
     
@@ -65,13 +62,13 @@ def XGBKVRegressor(params,  trainData,  nRounds, evalRounds=10,  metrics=["avera
     if "str" in str(type(metrics)):
         metrics=[metrics]
     for metric in metrics:
-        if (metric not in evalLookup) and (metric not in metrics1DLookup):
+        if (metric not in regEvalLookup) and (metric not in regMetrics1DLookup):
             metrics.remove(metric)
     
     def remetricise1D(mets):
         for i in range(len(mets)):
-            if mets[i] in metrics1DLookup:
-                mets= mets[0:i]+metrics1DLookup[mets[i]]+mets[i+1:len(mets)]
+            if mets[i] in regMetrics1DLookup:
+                mets= mets[0:i]+regMetrics1DLookup[mets[i]]+mets[i+1:len(mets)]
                 return remetricise1D(mets)
         return mets
     
@@ -123,8 +120,8 @@ def XGBKVRegressor(params,  trainData,  nRounds, evalRounds=10,  metrics=["avera
         metricLine=[nRounds-roundsToGo]
         idealLine=[None]
         for metric in expandedMetrics:
-            if metric in evalLookup:
-                eval_func=evalLookup[metric]
+            if metric in regEvalLookup:
+                eval_func=regEvalLookup[metric]
                 metricLine.append(eval_func(predicted, actual))
                 idealLine.append(eval_func(actual, actual))
             else:
@@ -185,7 +182,7 @@ def XGBKVRegressor(params,  trainData,  nRounds, evalRounds=10,  metrics=["avera
         #graphs for 1D metrics (bespoke enough that they need individual coding)
         
         if "quintiles" in metrics1D:
-            subMetrics=metrics1DLookup["quintiles"]
+            subMetrics=regMetrics1DLookup["quintiles"]
             relevantData=np.array([graphfodder[expandedMetrics.index(subMetric)+1] for subMetric in subMetrics])
             idealData=np.array([idealfodder[expandedMetrics.index(subMetric)+1] for subMetric in subMetrics])
             for i in range(len(relevantData[0])):
@@ -210,4 +207,165 @@ def XGBKVRegressor(params,  trainData,  nRounds, evalRounds=10,  metrics=["avera
     
     return bst
 
+#~~~CLASSIFIER~~~
 
+def guess_from_pred(pred):
+    if "int" in str(type(pred[0])) or "float" in str(type(pred[0])):
+        guess = pred
+    else:
+        guess = np.asarray([np.argmax(line) for line in pred])
+    return(guess)
+
+def provide_accuracy(pred,  act):
+    guess = guess_from_pred(pred)
+    rightcount = 0
+    for i in range(len(guess)):
+        if guess[i]==act[i]:
+            rightcount+=1
+    return float(rightcount)/len(guess)
+
+def provide_confusion_provider(x, y):
+    def provide_confusion(pred, act):
+        guess = guess_from_pred(pred)
+        rightcount = 0
+        for i in range(len(guess)):
+            if guess[i]==x and act[i]==y:
+                rightcount+=1
+        return rightcount
+    return provide_confusion
+
+claEvalLookup={
+"accuracy":provide_accuracy
+}
+
+def XGBKVClassifier(params,  trainData,  nRounds, evalRounds=10,  metrics=["accuracy", "confusion"],  testData=None, outputs=["terminal","csv",  "graphs"], csvName="output.csv",  graphPrefix="output", ideals=True):
+    
+    if testData==None:
+        testData=trainData
+    
+    #handle list(s) of metrics
+    
+    if "str" in str(type(metrics)):
+        metrics=[metrics]
+    for metric in metrics:
+        if (metric not in claEvalLookup and metric not in ["confusion"]):
+            metrics.remove(metric)
+            print(metric + " isn't a defined metric, sorry")
+    
+    expandedMetrics = metrics[:]
+    
+    if "confusion" in expandedMetrics:
+        expandedMetrics.remove("confusion")
+        if "num_class" in params:
+            for a in range(params["num_class"]):
+                for p in range(params["num_class"]):
+                    expandedMetrics.append("pred"+str(p)+" act"+str(a))
+                    claEvalLookup["pred"+str(p)+" act"+str(a)] = provide_confusion_provider(p, a)
+    
+    metrics0D=[metric for metric in metrics if metric in expandedMetrics]
+    
+    #rounds
+    
+    roundsToGo=nRounds
+    roundPlusMetrics = ["round"]+expandedMetrics
+    
+    #prep outputs
+    
+    if "terminal" in outputs:
+        params["silent"]=True
+        
+        template = " {0:10}"
+        for i in range(len(expandedMetrics)):
+            template+=" | {" + str(i+1)+":10}"
+        print(template.format(*roundPlusMetrics))
+        print ("------------"+"+------------"*len(expandedMetrics))
+    
+    if "csv" in outputs:
+        csv = open(csvName, "w")
+        csvLine=""
+        for item in roundPlusMetrics:
+            csvLine+=str(item)
+            csvLine+=","
+        csvLine=csvLine[:-1]
+        csvLine+="\n"
+        csv.write(csvLine)
+    
+    if "graphs" in outputs:
+        graphfodder=[[] for x in range(len(roundPlusMetrics))]
+        idealfodder=[[] for x in range(len(roundPlusMetrics))]
+    
+    bst=None
+    
+    while roundsToGo>0:
+        bst=xgb.train(params, trainData, min(evalRounds, roundsToGo), xgb_model=bst)
+        roundsToGo-=min(evalRounds, roundsToGo)
+        
+        predicted=bst.predict(testData)
+        actual = testData.get_label()
+        
+        #calculate the metrics
+        
+        metricLine=[nRounds-roundsToGo]
+        idealLine=[None]
+        for metric in expandedMetrics:
+            if metric in claEvalLookup:
+                eval_func=claEvalLookup[metric]
+                metricLine.append(eval_func(predicted, actual))
+                idealLine.append(eval_func(actual, actual))
+            else:
+                metricLine.append(None)
+                idealLine.append(None)
+        
+        #output to terminal
+        
+        if "terminal" in outputs:
+            printLine=[]
+            for item in metricLine:
+                if item==None:
+                    printLine.append("")
+                else:
+                    printLine.append(str(round(item, 8)))
+            print(template.format(*printLine))
+        
+        #output to csv
+        
+        if "csv" in outputs:
+            csvLine=""
+            for item in metricLine:
+                csvLine+=str(item)
+                csvLine+=","
+            csvLine=csvLine[:-1]
+            csvLine+="\n"
+            csv.write(csvLine)
+        
+        #add to graph output(s)
+        
+        if "graphs" in outputs:
+            for i in range(len(graphfodder)):
+                graphfodder[i].append(metricLine[i])
+                idealfodder[i].append(idealLine[i])
+    
+    #output graphs
+    
+    if "graphs" in outputs:
+        
+        #graphs for 0D metrics
+        
+        for i in range(1, len(graphfodder)):
+            if roundPlusMetrics[i] in metrics0D:
+                if ideals:
+                    plt.plot(graphfodder[0], idealfodder[i], "y")
+                    yellow_patch = mpatches.Patch(color='yellow', label='ideal')
+                    blue_patch = mpatches.Patch(color='blue', label='predicted')
+                    plt.legend(handles=[blue_patch, yellow_patch])
+                plt.plot(graphfodder[0], graphfodder[i],  "b.-")
+                
+                plt.xlim((0, max(graphfodder[0])*1.1))
+                plt.ylim((min(min(graphfodder[i])*1.1, 0), max(max(idealfodder[i]),  max(graphfodder[i]))*1.1))
+                plt.xlabel("rounds of training")
+                plt.ylabel(roundPlusMetrics[i])
+                plt.savefig(graphPrefix+"_"+roundPlusMetrics[i]+".png")
+                plt.clf()
+        
+    
+    return bst
